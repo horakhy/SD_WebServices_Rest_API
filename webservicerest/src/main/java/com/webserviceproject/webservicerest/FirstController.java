c54c2e44f903da87b62476cd92a32f4c61e38672
horakhy <leonidas51000@hotmail.com>
2022-05-29 15:17:14 -0300

    Project is finished :)

--- webservicerest/src/main/java/com/webserviceproject/webservicerest/FirstController.java
+++ webservicerest/src/main/java/com/webserviceproject/webservicerest/FirstController.java
@@ -2,7 +2,6 @@ package com.webserviceproject.webservicerest;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +13,18 @@ import org.json.JSONObject;
 @RestController
 public class FirstController {
 
+  // Titulos
+  private final String TITULO_PEGOU_RECURSO = "Recurso Adquirido";
+  private final String TITULO_LIBEROU_RECURSO = "Recurso Liberado";
+  private final String TITULO_LIVRE_RECURSO = "Recurso Livre";
+  private final String TITULO_OCUPADO_RECURSO = "Recurso Ocupado";
+
   // Mensagens
   private final String PEGOU_RECURSO_UM = "Você pegou o recurso 1";
   private final String PEGOU_RECURSO_DOIS = "Você pegou o recurso 2";
   private final String RECURSO_UM_OCUPADO = "O recurso 1 está ocupado, você foi adicionado na fila de espera";
   private final String RECURSO_DOIS_OCUPADO = "O recurso 2 está ocupado, você foi adicionado na fila de espera";
+  private final String RECURSO_JA_SOLICITADO = "Você já solicitou o recurso";
 
   private final String RECURSO_UM_INVALIDO = "Você não está utilizando o recurso 1";
   private final String RECURSO_DOIS_INVALIDO = "Você não está utilizando o recurso 2";
@@ -38,12 +44,12 @@ public class FirstController {
   private String clienteComRecurso_2 = null;
 
   // Disponibilidade dos recursos
-  private boolean recursoDisponível_1 = false;
-  private boolean recursoDisponível_2 = false;
+  private boolean recursoDisponível_1 = true;
+  private boolean recursoDisponível_2 = true;
 
   // Cliente em espera para o recurso 1 e 2
-  private ArrayList<String> clientesEmEspera1;
-  private ArrayList<String> clientesEmEspera2;
+  private ArrayList<String> clientesEmEspera1 = new ArrayList<String>();
+  private ArrayList<String> clientesEmEspera2 = new ArrayList<String>();
 
   // Timeout
   private static final int TEMPO_LIMITE = 5000; // Tempo em milisegundos
@@ -52,52 +58,62 @@ public class FirstController {
   public Map<String, SseEmitter> emitters = new HashMap<String, SseEmitter>();
 
   @CrossOrigin
-  @PostMapping(value="/solicita_recurso_um", consumes = MediaType.ALL_VALUE)
-  public String solicitaRecurso_1(@RequestParam String clientId) {
-    System.out.println("///////////////////////AS" + clientId);
-    String Client_id = clientId.toString();
-    String client_id_formatted = new JSONObject()
-        .put("clientId", clientId).toString();
-    System.out.println("///////////////////////AS" + client_id_formatted);
-    if (clienteComRecurso_1 == clientId) {
+  @PostMapping("/solicita_recurso_um")
+  public String solicitaRecurso_1(@RequestParam String client_id) {
+    if (client_id.equals(clienteComRecurso_1)) {
       return RECURSO_JA_OBTIDO;
     }
 
+    if (clientesEmEspera1.contains(client_id)) {
+      return RECURSO_JA_SOLICITADO;
+    }
+
     if (this.recursoDisponível_1) {
-      // Tempo para expirar o recurso
+
+      dispatchEventToClient(TITULO_PEGOU_RECURSO, PEGOU_RECURSO_UM, client_id);
+
       new Timer().schedule(new TimerTask() {
         @Override
         public void run() {
           try {
-            liberarRecursoTimeout_1(Client_id);
+            liberarRecursoTimeout_1(client_id);
           } catch (Exception e) {
             e.printStackTrace();
           }
         }
       }, TEMPO_LIMITE);
       this.recursoDisponível_1 = false;
-      this.clienteComRecurso_1 = clientId;
+      this.clienteComRecurso_1 = client_id;
 
       return PEGOU_RECURSO_UM;
     }
-    clientesEmEspera1.add(Client_id);
-    // client_id.notificar(RECURSO_UM_OCUPADO);
+    clientesEmEspera1.add(client_id);
+
+    dispatchEventToClient(TITULO_OCUPADO_RECURSO, RECURSO_UM_OCUPADO, client_id);
+
     return RECURSO_UM_OCUPADO;
   }
+
   @CrossOrigin
   @PostMapping("/solicita_recurso_dois")
-  public String solicitaRecurso_2(@RequestBody String client_id) {
-    if (clienteComRecurso_2 == client_id) {
+  public String solicitaRecurso_2(@RequestParam String client_id) {
+    if (client_id.equals(clienteComRecurso_2)) {
       return RECURSO_JA_OBTIDO;
     }
 
+    if (clientesEmEspera2.contains(client_id)) {
+      return RECURSO_JA_SOLICITADO;
+    }
+
     if (this.recursoDisponível_2) {
-      // Tempo para expirar o recurso
+
+      dispatchEventToClient(TITULO_PEGOU_RECURSO, PEGOU_RECURSO_DOIS, client_id);
+
       new Timer().schedule(new TimerTask() {
         @Override
         public void run() {
           try {
-            // liberarRecursoTimeout_1(client_id);
+            liberarRecursoTimeout_2(client_id);
           } catch (Exception e) {
             e.printStackTrace();
           }
@@ -109,7 +125,80 @@ public class FirstController {
       return PEGOU_RECURSO_DOIS;
     }
     clientesEmEspera2.add(client_id);
+
+    dispatchEventToClient(TITULO_OCUPADO_RECURSO, RECURSO_DOIS_OCUPADO, client_id);
+
     return RECURSO_DOIS_OCUPADO;
+  }
+
+  @CrossOrigin
+  @PostMapping("/libera_recurso_um")
+  public String liberaRecurso_1(@RequestParam String client_id) {
+    System.out
+        .println(
+            "O cliente " + client_id + " efetuou a liberação do recurso 1");
+    if (!client_id.equals(this.clienteComRecurso_1)) {
+      return RECURSO_UM_INVALIDO;
+    }
+
+    dispatchEventToClient(TITULO_LIBEROU_RECURSO, RECURSO_UM_LIBERADO, client_id);
+
+    if (clientesEmEspera1.size() > 0) {
+      String proximo_cliente = clientesEmEspera1.get(0);
+      this.clienteComRecurso_1 = proximo_cliente;
+      clientesEmEspera1.remove(0);
+
+      new Timer().schedule(new TimerTask() {
+        @Override
+        public void run() {
+          try {
+            liberarRecursoTimeout_1(proximo_cliente);
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+      }, TEMPO_LIMITE);
+      dispatchEventToClient(TITULO_LIVRE_RECURSO, RECURSO_UM_LIVRE, proximo_cliente);
+      return RECURSO_UM_LIBERADO;
+    }
+    this.recursoDisponível_1 = true;
+    this.clienteComRecurso_1 = null;
+    return STRING_VAZIA;
+  }
+
+  @CrossOrigin
+  @PostMapping("/libera_recurso_dois")
+  public String liberaRecurso_2(@RequestParam String client_id) {
+    System.out
+        .println(
+            "O cliente " + client_id + " efetuou a liberação do recurso 2");
+    if (!client_id.equals(this.clienteComRecurso_2)) {
+      return RECURSO_DOIS_INVALIDO;
+    }
+
+    dispatchEventToClient(TITULO_LIBEROU_RECURSO, RECURSO_DOIS_LIBERADO, client_id);
+
+    if (clientesEmEspera2.size() > 0) {
+      String proximo_cliente = clientesEmEspera2.get(0);
+      this.clienteComRecurso_2 = proximo_cliente;
+      clientesEmEspera2.remove(0);
+
+      new Timer().schedule(new TimerTask() {
+        @Override
+        public void run() {
+          try {
+            liberarRecursoTimeout_2(proximo_cliente);
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+      }, TEMPO_LIMITE);
+      dispatchEventToClient(TITULO_LIVRE_RECURSO, RECURSO_DOIS_LIVRE, proximo_cliente);
+      return RECURSO_DOIS_LIBERADO;
+    }
+    this.recursoDisponível_2 = true;
+    this.clienteComRecurso_2 = null;
+    return STRING_VAZIA;
   }
 
   public String liberarRecursoTimeout_1(String client_id) {
@@ -120,7 +209,8 @@ public class FirstController {
     if (this.clienteComRecurso_1 != null) {
       this.clienteComRecurso_1 = null;
       this.recursoDisponível_1 = true;
-      // return RECURSO_UM_EXPIRADO;
+
+      dispatchEventToClient(TITULO_LIBEROU_RECURSO, RECURSO_UM_EXPIRADO, client_id);
 
       if (clientesEmEspera1.size() > 0) {
         this.recursoDisponível_1 = false;
@@ -128,13 +218,12 @@ public class FirstController {
         this.clienteComRecurso_1 = proximo_cliente;
         clientesEmEspera1.remove(0);
 
-        // proximo_cliente.notificar(RECURSO_UM_LIVRE);
-
+        dispatchEventToClient(TITULO_LIVRE_RECURSO, RECURSO_UM_LIVRE, proximo_cliente);
         new Timer().schedule(new TimerTask() {
           @Override
           public void run() {
             try {
-              liberarRecursoTimeout_1(client_id);
+              liberarRecursoTimeout_1(proximo_cliente);
             } catch (Exception e) {
               e.printStackTrace();
             }
@@ -153,21 +242,21 @@ public class FirstController {
     if (this.clienteComRecurso_2 != null) {
       this.clienteComRecurso_2 = null;
       this.recursoDisponível_2 = true;
-      // return RECURSO_DOIS_EXPIRADO;
+
+      dispatchEventToClient(TITULO_LIBEROU_RECURSO, RECURSO_DOIS_EXPIRADO, client_id);
 
       if (clientesEmEspera2.size() > 0) {
         this.recursoDisponível_2 = false;
-        String proximo_cliente = clientesEmEspera1.get(0);
+        String proximo_cliente = clientesEmEspera2.get(0);
         this.clienteComRecurso_2 = proximo_cliente;
         clientesEmEspera2.remove(0);
 
-        // proximo_cliente.notificar(RECURSO_DOIS_LIVRE);
-
+        dispatchEventToClient(TITULO_LIVRE_RECURSO, RECURSO_DOIS_LIVRE, proximo_cliente);
         new Timer().schedule(new TimerTask() {
           @Override
           public void run() {
             try {
-              liberarRecursoTimeout_2(client_id);
+              liberarRecursoTimeout_2(proximo_cliente);
             } catch (Exception e) {
               e.printStackTrace();
             }
@@ -184,71 +273,6 @@ public class FirstController {
     } catch (IOException e) {
       e.printStackTrace();
     }
-  }
-
-  @PostMapping("/libera_recurso_um")
-  public String liberaRecurso_1(@RequestBody String client_id) {
-    System.out
-        .println(
-            "O cliente " + client_id + " efetuou a liberação do recurso 1");
-    if (client_id != this.clienteComRecurso_1) {
-      return RECURSO_UM_INVALIDO;
-    }
-
-    if (clientesEmEspera1.size() > 0) {
-      String proximo_cliente = clientesEmEspera1.get(0);
-      this.clienteComRecurso_1 = proximo_cliente;
-      clientesEmEspera1.remove(0);
-      dispatchEventToClients("Titulo yooo", RECURSO_UM_LIVRE, client_id);
-
-      // proximo_cliente.notificar(RECURSO_UM_LIVRE);
-      new Timer().schedule(new TimerTask() {
-        @Override
-        public void run() {
-          try {
-            liberarRecursoTimeout_1(proximo_cliente);
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
-        }
-      }, TEMPO_LIMITE);
-      return RECURSO_UM_LIBERADO;
-    }
-    this.recursoDisponível_1 = true;
-    this.clienteComRecurso_1 = null;
-    return RECURSO_UM_LIBERADO;
-  }
-
-  @PostMapping("/libera_recurso_dois")
-  public String liberaRecurso_2(@RequestBody String client_id) {
-    System.out
-        .println(
-            "O cliente " + client_id + " efetuou a liberação do recurso 2");
-    if (client_id != this.clienteComRecurso_2) {
-      return RECURSO_DOIS_INVALIDO;
-    }
-
-    if (clientesEmEspera2.size() > 0) {
-      String proximo_cliente = clientesEmEspera2.get(0);
-      this.clienteComRecurso_2 = proximo_cliente;
-      clientesEmEspera2.remove(0);
-      dispatchEventToClients("Titulo yooo", RECURSO_DOIS_LIVRE, client_id);
-      // proximo_cliente.notificar(RECURSO_DOIS_LIVRE);
-      new Timer().schedule(new TimerTask() {
-        @Override
-        public void run() {
-          try {
-            liberarRecursoTimeout_2(proximo_cliente);
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
-        }
-      }, TEMPO_LIMITE);
-      return RECURSO_DOIS_LIBERADO;
-    }
-    this.recursoDisponível_2 = true;
-    this.clienteComRecurso_2 = null;
-    return RECURSO_DOIS_LIBERADO;
   }
 
   // Método para a inscrição do cliente
@@ -276,9 +300,7 @@ public class FirstController {
   }
 
   // Método para o dispatch do evento para cliente específico
-  @PostMapping(value = "/dispatchEvent")
-  public void dispatchEventToClients(@RequestParam String title, @RequestParam String text,
-      @RequestParam String userId) {
+  public void dispatchEventToClient(String title, String text, String userId) {
     String eventFormatted = new JSONObject()
         .put("title", title)
         .put("text", text).toString();
@@ -292,19 +314,4 @@ public class FirstController {
       }
     }
   }
-
 }
-// if (client_id != this.clienteComRecurso_1) {
-// System.out
-// .println(
-// "O cliente " + client_id + " registrou interesse no recurso " + numRecurso);
-
-// if (numRecurso == 1)
-// return solicitaRecurso_1(client_id);
-
-// }
-// if (client_id != this.clienteComRecurso_2) {
-// if (numRecurso == 2)
-// return solicitaRecurso_2(client_id);
-// }
-// return RECURSO_JA_OBTIDO;
